0d76cbe789124bfa18fde02544ed89310201c257
Aleksei Riasnov <aleksei_riasnov@epam.com>
2018-09-17 11:46:01 +0400

    methods realizatoin

--- src/main/java/ru/sstu/controller/MainController.java
+++ src/main/java/ru/sstu/controller/MainController.java
@@ -3,11 +3,13 @@ package ru.sstu.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import ru.sstu.model.Matrix;
 import ru.sstu.service.GraphicCore;
 import ru.sstu.service.MatrixService;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,6 +24,9 @@ public class MainController implements Initializable {
     @FXML
     private Pane resultMatrix;
 
+    @FXML
+    private Label determinantLabel;
+
     private Matrix firstMatrixData;
 
     private Matrix secondMatrixData;
@@ -32,8 +37,8 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        firstMatrixData = new Matrix(2);
-        secondMatrixData = new Matrix(2);
+        firstMatrixData = new Matrix(1);
+        secondMatrixData = new Matrix(1);
         graphic = new GraphicCore();
         matrixService = new MatrixService();
         graphic.drawMatrix(firstMatrix, firstMatrixData);
@@ -65,21 +70,54 @@ public class MainController implements Initializable {
     }
 
     public void sum(ActionEvent actionEvent) {
-        updateData(firstMatrix, firstMatrixData);
-        updateData(secondMatrix, secondMatrixData);
-        //updateData(resultMatrix, resultMatrixData);
+        updateData();
         Matrix m = matrixService.sum(firstMatrixData, secondMatrixData);
         graphic.drawMatrix(resultMatrix, m);
+    }
+
+    public void diff(ActionEvent actionEvent) {
+        updateData();
+        Matrix result = matrixService.difference(firstMatrixData, secondMatrixData);
+        graphic.drawMatrix(resultMatrix, result);
+    }
+
+    public void mult(ActionEvent actionEvent) {
+        updateData();
+        Matrix result = matrixService.multiply(firstMatrixData, secondMatrixData);
+        graphic.drawMatrix(resultMatrix, result);
+    }
+
+    public void determinant(ActionEvent actionEvent) {
+        updateData();
+        Double determinant = matrixService.determinant(firstMatrixData);
+        determinantLabel.setText(" Determinant: " + determinant);
+    }
+
+    public void invert(ActionEvent actionEvent) {
+        updateData();
+        Matrix result = matrixService.invert(firstMatrixData);
+        graphic.drawMatrix(resultMatrix, result);
+    }
+
+    public void choleskyDecomposition(ActionEvent actionEvent) {
+        updateData();
+        Matrix result = matrixService.choleskyDecomposition(firstMatrixData);
+        graphic.drawMatrix(resultMatrix, result);
     }
 
     private void updateData(Pane pane, Matrix data) {
         Integer count = 0;
         for (int i = 0; i < data.height(); i++) {
             for (int j = 0; j < data.width(); j++) {
-                TextField field = (TextField)pane.getChildren().get(count);
+                TextField field = (TextField) pane.getChildren().get(count);
                 data.setElement(i, j, Integer.parseInt(field.getText()));
                 count++;
             }
         }
+    }
+
+    protected void updateData() {
+        updateData(firstMatrix, firstMatrixData);
+        updateData(secondMatrix, secondMatrixData);
     }
 }
--- src/main/java/ru/sstu/service/MatrixService.java
+++ src/main/java/ru/sstu/service/MatrixService.java
@@ -6,6 +6,7 @@ import ru.sstu.model.Matrix;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 /**
@@ -13,32 +14,80 @@ import java.util.stream.Stream;
  */
 public class MatrixService {
 
+    private static final String RESULT = "result";
+
     public Matrix sum(Matrix firstMatrix, Matrix secondMatrix) {
+        return operation(firstMatrix, secondMatrix, "matrix_sum");
+    }
+
+    public Matrix difference(Matrix firstMatrix, Matrix secondMatrix) {
+        return operation(firstMatrix, secondMatrix, "matrix_diff");
+    }
+
+    public Matrix multiply(Matrix firstMatrix, Matrix secondMatrix) {
+        return operation(firstMatrix, secondMatrix, "matrix_multiply");
+    }
+
+    /**
+     * Determinant could be calculated only for square matrices
+     */
+    public Double determinant(Matrix matrix) {
+        Query query = new Query("consult('matrix.pl')");
+
+        if (query.hasSolution()) {
+            Query q2 = new Query("determinant(" + matrix.asString() + RESULT + ")");
+            if (q2.hasMoreSolutions()) {
+                Map<String, Term> sol = q2.nextSolution();
+                return sol.get(RESULT).doubleValue();
+            }
+            q2.close();
+        }
+        return null;
+    }
+
+    /**
+     * positive semi-definite matrix have condition
+     * xT * A * x > 0.
+     * <p>
+     * [2, -1, 0]      (x1)
+     * (x1, x2, x3)  *  [-1, 2, -1]  *  (x2)  => 2 * (x1 - x2/2)^2 + 3/2*(x2-2*x3/3)^2 + 2/3*x3^2 > 0.
+     * [0, -1, 2]      (x3)
+     * <p>
+     * Матрица положительно полуопределена, если выполняются условия:
+     * 1) Все диагональные элементы неотрицательны;
+     * 2) Все главные определители неотрицательны.
+     */
+    public Matrix invert(Matrix matrix) {
+        return operation(matrix, "matrix_inversion");
+    }
+
+    public Matrix choleskyDecomposition(Matrix matrix) {
+        return operation(matrix, "cholesky_decomposition");
+    }
+
+    protected Matrix operation(Matrix matrix, String operation) {
         Query query = new Query("consult('matrix.pl')");
         Matrix resultMatrix = new Matrix();
         if (query.hasSolution()) {
-            Query q5 = new Query("matrix_sum(" + firstMatrix.asString() + secondMatrix.asString() + "M).");
+            Query q5 = new Query(operation + "(" + matrix.asString() + RESULT + ").");
             q5.open();
-            Stream.of(q5.getSolution().get("M").toTermArray()).forEach(term -> resultMatrix.fillRow(getValue(term)));
+            Stream.of(q5.getSolution().get(RESULT).toTermArray()).forEach(term -> resultMatrix.fillRow(getValue(term)));
             q5.close();
         }
         return resultMatrix;
     }
 
-    public Matrix difference(Matrix firstMatrix, Matrix secondMatrix) {
-        return new Matrix();
-    }
-
-    public Matrix multiply(Matrix firstMatrix, Matrix secondMatrix) {
-        return new Matrix();
-    }
-
-    public void determinant(Matrix matrix) {
-
-    }
-
-    public void invert(Matrix matrix) {
-
+    protected Matrix operation(Matrix firstMatrix, Matrix secondMatrix, String operation) {
+        Query query = new Query("consult('matrix.pl')");
+        Matrix resultMatrix = new Matrix();
+        if (query.hasSolution()) {
+            Query q5 = new Query(operation + "(" + firstMatrix.asString() + secondMatrix.asString() + RESULT + ").");
+            q5.open();
+            Stream.of(q5.getSolution().get(RESULT).toTermArray())
+                    .forEach(term -> resultMatrix.fillRow(getValue(term)));
+            q5.close();
+        }
+        return resultMatrix;
     }
 
     protected List<Integer> getValue(Term term) {
